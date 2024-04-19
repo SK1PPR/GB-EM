40d3f4f55e896e65d3879ceebdb451a3dfc34052
Ann18532 <anupsavaliya28@gmail.com>
2024-04-19 16:39:55 +0530

    Added lib, timer and device

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+#![crate_name = "rboy"]
+#![crate_type = "lib" ]
+
+pub use crate::keypad::KeypadKey;
+pub use crate::gpu::{SCREEN_W, SCREEN_H};
+pub use crate::sound::AudioPlayer;
+
+pub mod device;
+
+mod cpu;
+mod gbmode;
+mod gpu;
+mod keypad;
+mod mbc;
+mod mmu;
+mod printer;
+mod register;
+mod serial;
+mod sound;
+mod timer;
+
+pub type StrResult<T> = Result<T, &'static str>;
